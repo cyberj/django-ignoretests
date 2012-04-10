f40b50eeda9543130e10576139a1323d887c744d
Charpentier Johan <cyberj@arcagenis.org>
2012-04-10 09:26:10 +0200

    Tag 0.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys, os
 
 from setuptools import setup, find_packages
 
-version = '0.3'
+version = '0.3.1'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
